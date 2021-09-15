dbff30f53cbbc71304dd587626060678df1bf75f
prioarief <prioariefgunawangunawan@gmail.com>
2021-09-15 16:01:40 +0700

    refactor get distributor endpoint

--- src/services/Distributor.ts
+++ src/services/Distributor.ts
@@ -1,50 +1,68 @@
 import { Request } from "express";
+import { QueryTypes } from "sequelize";
 import db from "../config/db";
 
 class Distributor {
-  get(req: Request): any {
-    const {
-      outlet_id,
-      area_id,
-      region_id,
-      wilayah_id,
-      ass_id,
-      asm_id,
-      salesman_id,
+  async get(req: Request): Promise<any> {
+    let {
+        outlet_id,
+        area_id,
+        wilayah_id,
+        distributor_id,
+        region_id,
+        ass_id,
+        asm_id,
+        salesman_id,
     } = req.validated;
-    const {scope, level} = req.body.decoded
-		let addWhere : string = ''
-		if(level === "distributor_manager") addWhere = 'o.distributor_id'
-		if(level === "region_manager") addWhere = 'o.region_id'
-		if(level === "area_manager") addWhere = 'o.city_id_alias'
-		if(level === "outlet_manager") addWhere = 'o.outlet_id'
-    const query = db()
-      .select("d.distributor_name")
-      .distinct("d.distributor_id")
-      .from("mstr_distributor as d")
-      .innerJoin(
-        "mstr_outlet as o",
-        "d.distributor_id",
-        "o.distributor_id"
-      )
-      .innerJoin("ms_pulau_alias as r", "o.region_id", "r.pulau_id_alias")
-      .innerJoin("ms_user_scope as us", "o.outlet_id", "us.scope")
-      .innerJoin(
-        "ms_dist_pic as pic",
-        "o.distributor_id",
-        "pic.distributor_id"
-      )
-      .where({
-        ...(outlet_id && { "o.outlet_id": outlet_id }),
-        ...(area_id && { "o.city_id_alias": area_id }),
-        ...(region_id && { "o.region_id": region_id }),
-        ...(wilayah_id && { "r.head_region_id": wilayah_id }),
-        ...(ass_id && { "pic.ass_id": ass_id }),
-        ...(asm_id && { "pic.asm_id": asm_id }),
-        ...(salesman_id && { "us.user_id": salesman_id }),
-      }).whereIn(addWhere, scope.split(','))
-      .orderBy("distributor_id");
-    return query;
+    const { scope, level } = req.body.decoded;
+    let addWhere: string | null = null;
+    let params: string[] = [];
+    if (level === '4') addWhere = 'o.distributor_id';
+    if (level === '2') addWhere = 'o.region_id';
+    if (level === '3') addWhere = 'o.city_id_alias';
+    if (level === '5') addWhere = 'o.outlet_id';
+
+    let query =
+        'select distinct d.distributor_name, d.distributor_id from mstr_distributor as d INNER JOIN mstr_outlet as o on d.distributor_id = o.distributor_id INNER JOIN ms_pulau_alias as r on o.region_id = r.pulau_id_alias INNER JOIN ms_user_scope as us on o.outlet_id = us.scope INNER JOIN ms_dist_pic as dp on o.distributor_id = dp.distributor_id WHERE d.distributor_id IS NOT NULL';
+
+    if (addWhere) {
+        query += ' AND ' + addWhere + ' IN (?)';
+        params.push(scope);
+    }
+    if (distributor_id) {
+        query += ' AND o.distributor_id = ?';
+        params.push(distributor_id);
+    }
+    if (region_id) {
+        query += ' AND o.region_id = ?';
+        params.push(region_id);
+    }
+    if (outlet_id) {
+        query += ' AND o.outlet_id = ?';
+        params.push(outlet_id);
+    }
+    if (area_id) {
+        query += ' AND o.city_id_alias = ?';
+        params.push(area_id);
+    }
+    if (wilayah_id) {
+        query += ' AND r.head_region_id = ?';
+        params.push(wilayah_id);
+    }
+    if (ass_id) {
+        query += ' AND dp.ass_id = ?';
+        params.push(ass_id);
+    }
+    if (asm_id) {
+        query += ' AND dp.asm_id = ?';
+        params.push(asm_id);
+    }
+
+    return await db.query(query + ' order by d.distributor_id ASC', {
+        raw: true,
+        type: QueryTypes.SELECT,
+        replacements: params,
+    });
   }
 }
 
