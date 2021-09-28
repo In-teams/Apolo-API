fbe6392e1859e703c255f56a20efbdee120326bd
prioarief <prioariefgunawangunawan@gmail.com>
2021-09-28 11:48:40 +0700

    remove unused code

--- src/controllers/Registration.ts
+++ src/controllers/Registration.ts
@@ -4,26 +4,6 @@ import response from "../helpers/Response";
 import Service from "../services/Registration";
 
 class Registration {
-  // async post(req: Request, res: Response): Promise<object | undefined> {
-  //   const path = req.validated.path
-  //   try {
-  //     await Service.post(req);
-  //     return response(res, true, "success post registration form", null, 200);
-  //   } catch (error) {
-  //     await FileSystem.DeleteFile(path)
-  //     return response(res, false, null, JSON.stringify(error), 500);
-  //   }
-  // }
-  // async validation(req: Request, res: Response): Promise<object | undefined> {
-  //   const path = req.validated.path
-  //   try {
-  //     await Service.validation(req);
-  //     return response(res, true, "success post registration form", null, 200);
-  //   } catch (error) {
-  //     await FileSystem.DeleteFile(path)
-  //     return response(res, false, null, JSON.stringify(error), 500);
-  //   }
-  // }
   async get(req: Request, res: Response): Promise<object | undefined> {
     try {
       const regist: any[] = await Service.getRegistrationSummary(req);
@@ -40,35 +20,6 @@ class Registration {
       return response(res, false, null, JSON.stringify(error), 500);
     }
   }
-//   async getLastRegistration(
-//     req: Request,
-//     res: Response
-//   ): Promise<object | undefined> {
-//     try {
-//       const regist: any[] = await Service.getLastRegistration(req);
-//       return response(res, true, regist, null, 200);
-//     } catch (error) {
-//       return response(res, false, null, JSON.stringify(error), 500);
-//     }
-//   }
-//   async getRegistrationSummary(
-//     req: Request,
-//     res: Response
-//   ): Promise<object | undefined> {
-//     try {
-//       let regist: any[] = await Service.getRegistationSummaryByHeadRegion(req);
-//       regist = regist.map((val: any) => ({
-//         ...val,
-//         total: val.regist + val.notregist,
-//         percentage:
-//           ((val.regist / (val.regist + val.notregist)) * 100).toFixed(2) + "%",
-//       }));
-
-//       return response(res, true, regist, null, 200);
-//     } catch (error) {
-//       return response(res, false, null, JSON.stringify(error), 500);
-//     }
-//   }
 }
 
 export default new Registration();
--- src/services/Redeem.ts
+++ src/services/Redeem.ts
@@ -12,145 +12,22 @@ let getPointRedeemQuery =
 
 class Redeem {
   async getPointSummary(req: Request): Promise<any> {
-    let {query: pq, params: pp} = FilterParams.aktual(req, getPointQuery);
-    let {query: prq, params: prp} = FilterParams.aktual(req, getPointRedeemQuery);
+    let { query: pq, params: pp } = FilterParams.aktual(req, getPointQuery);
+    let { query: prq, params: prp } = FilterParams.aktual(
+      req,
+      getPointRedeemQuery
+    );
 
-    let q = `SELECT DISTINCT (${pq}) AS achieve, (${prq}) AS redeem FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS reg ON o.region_id = reg.pulau_id_alias INNER JOIN ms_head_region AS mhr ON mhr.head_region_id = reg.head_region_id INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL`
+    let q = `SELECT DISTINCT (${pq}) AS achieve, (${prq}) AS redeem FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS reg ON o.region_id = reg.pulau_id_alias INNER JOIN ms_head_region AS mhr ON mhr.head_region_id = reg.head_region_id INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL`;
 
-    let {query, params} = FilterParams.query(req, q)
+    let { query, params } = FilterParams.query(req, q);
 
     return await db.query(query, {
-        raw: true,
-        type: QueryTypes.SELECT,
-        replacements: [...pp, ...prp, ...params],
-      });
+      raw: true,
+      type: QueryTypes.SELECT,
+      replacements: [...pp, ...prp, ...params],
+    });
   }
-  // getPointSummaryByHR(req: Request): any {
-  // 	const {
-  // 		distributor_id,
-  // 		outlet_id,
-  // 		area_id,
-  // 		region_id,
-  // 		wilayah_id,
-  // 		ass_id,
-  // 		asm_id,
-  // 		salesman_id,
-  // 		sort,
-  // 	} = req.validated;
-  // 	const query = db()
-  // 		.select(
-  // 			'hr.head_region_name as wilayah',
-  // 			this.getPoint(req)
-  // 				.where('r.head_region_id', '=', db().raw('hr.head_region_id'))
-  // 				.as('achieve'),
-  // 			this.getPointRedeem(req)
-  // 				.where('r.head_region_id', '=', db().raw('hr.head_region_id'))
-  // 				.as('redeem')
-  // 		)
-  // 		.from('mstr_outlet as o')
-  // 		.innerJoin('ms_pulau_alias as r', 'o.region_id', 'r.pulau_id_alias')
-  // 		.innerJoin(
-  // 			'ms_head_region as hr',
-  // 			'r.head_region_id',
-  // 			'hr.head_region_id'
-  // 		)
-  // 		.innerJoin('ms_dist_pic as pic', 'o.distributor_id', 'pic.distributor_id')
-  // 		// .innerJoin("ms_user_scope", "mstr_outlet.outlet_id", "ms_user_scope.scope")
-  // 		// .innerJoin("ms_user", "ms_user_scope.user_id", "ms_user.user_id")
-  // 		.where({
-  // 			...(distributor_id && { 'o.distributor_id': distributor_id }),
-  // 			...(outlet_id && { 'o.outlet_id': outlet_id }),
-  // 			...(area_id && { 'o.city_id_alias': area_id }),
-  // 			...(region_id && { 'o.region_id': region_id }),
-  // 			...(wilayah_id && { 'r.head_region_id': wilayah_id }),
-  // 			...(ass_id && { 'pic.ass_id': ass_id }),
-  // 			...(asm_id && { 'pic.asm_id': asm_id }),
-  // 			// ...(salesman_id && { "ms_user.user_id": salesman_id }),
-  // 		})
-  // 		.groupBy('wilayah')
-  // 		.orderBy('achieve', sort);
-  // 	// console.log(query.toSQL().sql);
-  // 	return query;
-  // }
-  // async post(req: Request): Promise<any> {
-  // 	try {
-  // 		const { outlet_id, filename } = req.validated;
-  // 		return await db().transaction(async (trx) => {
-  // 			const insert = await trx('trx_file_penukaran').insert({
-  // 				outlet_id,
-  // 				filename,
-  // 			});
-
-  // 			await trx('trx_history_penukaran').insert({
-  // 				outlet_id,
-  // 				file_id: insert[0],
-  // 			});
-  // 		});
-  // 	} catch (error) {
-  // 		console.log(error);
-  // 	}
-  // }
-  // async validation(req: Request): Promise<any> {
-  // 	try {
-  // 		const { status_penukaran, id, outlet_id } = req.validated;
-  // 		return await db().transaction(async (trx) => {
-  // 			await trx('trx_file_penukaran')
-  // 				.where({ id })
-  // 				.update({
-  // 					status_penukaran,
-  // 					validated_at: DateFormat.getToday('YYYY-MM-DD HH:mm:ss'),
-  // 				});
-  // 			await trx('trx_history_penukaran').insert({
-  // 				outlet_id,
-  // 				status_penukaran,
-  // 				file_id: id,
-  // 			});
-  // 		});
-  // 	} catch (error) {
-  // 		console.log(error);
-  // 	}
-  // }
-  // update(req: Request): any {
-  // 	const { id } = req.validated;
-  // 	delete req.validated.id;
-  // 	return db()('trx_file_penukaran').where({ id }).update(req.validated);
-  // }
-  // getRedeemForm(req: Request): any {
-  // 	const { outlet_id } = req.validated;
-  // 	return db()
-  // 		.select('*')
-  // 		.from('trx_file_penukaran')
-  // 		.where({ outlet_id })
-  // 		.andWhereRaw('MONTH(uploaded_at) = ?', [new Date().getMonth() + 1]);
-  // }
-  // getLastTrCode(): any {
-  // 	return db()("trx_transaksi_redeem").max('kd_transaksi as kd_transaksi')
-  // }
-  // getProduct(req: Request): any {
-  // 	const { point, product_id, category } = req.validated;
-  // 	const query = db()
-  // 		.select('mp.product_id', 'mp.product_name', 'mpb.point', 'mp.category')
-  // 		.from('ms_product as mp')
-  // 		.innerJoin('ms_program_barang as mpb', 'mp.product_id', 'mpb.product_id')
-  // 		.where({
-  // 			...(category && { category }),
-  // 		});
-  // 	if (point) query.whereRaw('point <= ?', [point]);
-  // 	if (product_id) query.whereIn('mp.product_id', product_id);
-
-  // 	return query;
-  // }
-  // async checkout(req: Request): Promise<any> {
-  // 	try {
-  // 	  return await db().transaction(async (trx) => {
-  // 		await trx("trx_transaksi_redeem").insert(req.validated.data)
-  // 		await trx("trx_transaksi_redeem_barang").insert(req.validated.detail)
-  // 	  });
-  // 	  // return db()("trx_file_registrasi").insert(req.validated);
-  // 	} catch (error) {
-  // 	  console.log(error, "<<<");
-  // 	}
-  //   }
 }
 
 export default new Redeem();
