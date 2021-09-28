84d640ed6282b5067021b155b8e8a402256eee56
prioarief <prioariefgunawangunawan@gmail.com>
2021-09-28 13:53:08 +0700

    fix get registration summary by head region and sort it

--- src/services/Registration.ts
+++ src/services/Registration.ts
@@ -19,11 +19,16 @@ class Registration {
     });
   }
   async getRegistrationSummaryByHR(req: Request): Promise<any> {
-    let { query: qoc, params: poc } = FilterParams.query(req, queryOutletCount + " AND o.valid IN ('No', 'No+') AND r.head_region_id = mhr.head_region_id");
-    let q = `SELECT mhr.head_region_name as wilayah, (${qoc}) AS notregist, COUNT(o.outlet_id) AS regist FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS reg ON o.region_id = reg. pulau_id_alias INNER JOIN ms_head_region AS mhr ON mhr.head_region_id = reg.head_region_id INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL AND o.valid NOT IN ('No', 'No+')`;
+    const { sort } = req.validated;
+    let { query: qoc, params: poc } = FilterParams.query(
+      req,
+      queryOutletCount +
+        " AND o.valid IN ('No', 'No+') AND r.head_region_id = mhr.head_region_id"
+    );
+    let q = `SELECT mhr.head_region_name as wilayah, (${qoc}) AS notregist, COUNT(o.outlet_id) AS regist, TRUNCATE((COUNT(o.outlet_id)/((${qoc}) + COUNT(o.outlet_id)) * 100), 2) AS pencapaian FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS reg ON o.region_id = reg. pulau_id_alias INNER JOIN ms_head_region AS mhr ON mhr.head_region_id = reg.head_region_id INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL AND o.valid NOT IN ('No', 'No+')`;
 
     let { query, params } = FilterParams.query(req, q);
-    return await db.query(query + " GROUP BY wilayah", {
+    return await db.query(query + ` GROUP BY wilayah ORDER BY pencapaian ${sort}`, {
       raw: true,
       type: QueryTypes.SELECT,
       replacements: [...poc, ...params],
