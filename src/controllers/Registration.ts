7fb0829c33135ed48ec3eb5c723ff97ae4baf097
prioarief <prioariefgunawangunawan@gmail.com>
2021-09-28 13:24:22 +0700

    get registration summary by head region

--- src/controllers/Registration.ts
+++ src/controllers/Registration.ts
@@ -20,6 +20,22 @@ class Registration {
       return response(res, false, null, JSON.stringify(error), 500);
     }
   }
+  async getRegistrationSummary(req: Request, res: Response): Promise<object | undefined> {
+    try {
+      const regist: any[] = await Service.getRegistrationSummaryByHR(req);
+      // const { total, total_outlet } = regist[0];
+      // const result: object = {
+      //   regist: regist[0].total,
+      //   percentage: ((total / total_outlet) * 100).toFixed(2) + "%",
+      //   percen: parseFloat(((total / total_outlet) * 100).toFixed(2)),
+      //   notregist: total_outlet - total,
+      //   totalOutlet: total_outlet,
+      // };
+      return response(res, true, regist, null, 200);
+    } catch (error) {
+      return response(res, false, null, JSON.stringify(error), 500);
+    }
+  }
 }
 
 export default new Registration();
--- src/services/Registration.ts
+++ src/services/Registration.ts
@@ -1,9 +1,7 @@
 import { Request } from "express";
 import { QueryTypes } from "sequelize";
 import db from "../config/db";
-import DateFormat from "../helpers/DateFormat";
 import FilterParams from "../helpers/FilterParams";
-import OutletService from "./Outlet";
 
 let queryOutletCount =
   "SELECT COUNT(DISTINCT o.outlet_id) AS total FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS r ON o.`region_id` = r. `pulau_id_alias` INNER JOIN ms_dist_pic AS dp ON o.`distributor_id` = dp.`distributor_id` WHERE o.`outlet_id` IS NOT NULL";
@@ -13,12 +11,23 @@ class Registration {
     let { query: qoc, params: poc } = FilterParams.query(req, queryOutletCount);
     let q = `SELECT (${qoc}) AS total_outlet, COUNT(DISTINCT o.outlet_id) AS total FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS r ON o.region_id = r. pulau_id_alias INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL AND o.valid NOT IN ('No', 'No+')`;
 
-	let {query, params} = FilterParams.query(req, q)
-	return await db.query(query, {
-		raw: true,
-		type: QueryTypes.SELECT,
-		replacements: [...poc, ...params],
-	  });
+    let { query, params } = FilterParams.query(req, q);
+    return await db.query(query, {
+      raw: true,
+      type: QueryTypes.SELECT,
+      replacements: [...poc, ...params],
+    });
+  }
+  async getRegistrationSummaryByHR(req: Request): Promise<any> {
+    let { query: qoc, params: poc } = FilterParams.query(req, queryOutletCount + " AND o.valid IN ('No', 'No+') AND r.head_region_id = mhr.head_region_id");
+    let q = `SELECT mhr.head_region_name as wilayah, (${qoc}) AS notregist, COUNT(o.outlet_id) AS regist FROM mstr_outlet AS o INNER JOIN ms_pulau_alias AS reg ON o.region_id = reg. pulau_id_alias INNER JOIN ms_head_region AS mhr ON mhr.head_region_id = reg.head_region_id INNER JOIN ms_dist_pic AS dp ON o.distributor_id = dp.distributor_id WHERE o.outlet_id IS NOT NULL AND o.valid NOT IN ('No', 'No+')`;
+
+    let { query, params } = FilterParams.query(req, q);
+    return await db.query(query + " GROUP BY wilayah", {
+      raw: true,
+      type: QueryTypes.SELECT,
+      replacements: [...poc, ...params],
+    });
   }
 }
 
