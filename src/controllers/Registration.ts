09d40ae50552213db656fcef93312fdb50372a02
prioarief <prioariefgunawangunawan@gmail.com>
2021-09-30 13:15:58 +0700

    prepare upload registration form

--- src/controllers/Registration.ts
+++ src/controllers/Registration.ts
@@ -5,6 +5,22 @@ import response from "../helpers/Response";
 import Service from "../services/Registration";
 
 class Registration {
+  async post(req: Request, res: Response): Promise<object | undefined> {
+    try {
+      const regist: any[] = await Service.getRegistrationSummary(req);
+      const { total, total_outlet } = regist[0];
+      const result: object = {
+        regist: regist[0].total,
+        percentage: ((total / total_outlet) * 100).toFixed(2) + "%",
+        percen: parseFloat(((total / total_outlet) * 100).toFixed(2)),
+        notregist: total_outlet - total,
+        totalOutlet: total_outlet,
+      };
+      return response(res, true, result, null, 200);
+    } catch (error) {
+      return response(res, false, null, JSON.stringify(error), 500);
+    }
+  }
   async get(req: Request, res: Response): Promise<object | undefined> {
     try {
       const regist: any[] = await Service.getRegistrationSummary(req);
--- src/request/Registration.ts
+++ src/request/Registration.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import joi from "joi";
-// import { pathRegistration } from "../config/app";
+import config from "../config/app";
 import FileSystem from "../helpers/FileSystem";
 import GetFileExtention from "../helpers/GetFileExtention";
 import response from "../helpers/Response";
-// import PeriodeService from "../services/Periode";
+import PeriodeService from "../services/Periode";
 import OutletService from "../services/Outlet";
-// import RegistrationService from "../services/Registration";
+import RegistrationService from "../services/Registration";
 
 class Registration {
   get(req: Request, res: Response, next: NextFunction): any {
@@ -31,80 +31,66 @@ class Registration {
     req.validated = { ...value, sort: value.sort || "ASC" };
     next();
   }
-//   async post(req: Request, res: Response, next: NextFunction): Promise<any> {
-//     try {
-//       const schema = joi.object({
-//         file: joi.string().base64().required(),
-//         outlet_id: joi.string().required(),
-//         type_file: joi.string().required(),
-//         ektp: joi.string(),
-//         npwp: joi.string(),
-//         nama_konsumen: joi.string().required(),
-//         telepon1: joi.string().required(),
-//         alamat1: joi.string(),
-//         rtrw: joi.string(),
-//         kodepos: joi.string(),
-//         propinsi: joi.string(),
-//         kabupaten: joi.string(),
-//         kecamatan: joi.string(),
-//         kelurahan: joi.string(),
-//         nomor_rekening: joi.string().required(),
-//         nama_rekening: joi.string().required(),
-//         cabang_bank: joi.string().required(),
-//         nama_bank: joi.string().required(),
-//         kota_bank: joi.string().required(),
-//       });
+  async post(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const schema = joi.object({
+        file: joi.string().base64().required(),
+        outlet_id: joi.string().required()
+      });
 
-//       const { value, error } = schema.validate(req.body);
-//       if (error) {
-//         return response(res, false, null, error.message, 400);
-//       }
+      const { value, error } = schema.validate(req.body);
+      if (error) {
+        return response(res, false, null, error.message, 400);
+      }
 
-//       req.validated = value;
-//       const check = await PeriodeService.checkData(req);
-//       if (check.length < 1)
-//         return response(res, false, null, "bukan periode upload", 400);
-//       const ext = GetFileExtention(value.file);
-//       let { periode, id: periode_id } = check[0];
-//       periode = `p-${periode.split(" ")[1]}`;
-//       const filename = `${periode}-${value.type_file}-${Date.now()}-${
-//         value.outlet_id
-//       }${ext}`;
-//       const path = pathRegistration + "/" + filename;
-//       req.validated = { ...req.validated, filename, periode_id, path };
-//       delete req.validated.file;
-//       const uploaded = await RegistrationService.getRegistrationForm(req);
-//       if (uploaded.length > 0) {
-//         const { status_registrasi: status } = uploaded[0];
-//         if (status === 7 || status === 8) {
-//           return response(
-//             res,
-//             false,
-//             null,
-//             "Registration form was validated",
-//             400
-//           );
-//         } else {
-//           const { id, filename } = uploaded[0];
-//           await FileSystem.DeleteFile(`${pathRegistration}/${filename}`);
-//           await FileSystem.WriteFile(path, value.file, true);
-//           req.validated.id = id;
-//           await RegistrationService.update(req);
-//           return response(
-//             res,
-//             true,
-//             "success post registration form",
-//             null,
-//             200
-//           );
-//         }
-//       }
-//       await FileSystem.WriteFile(path, value.file, true);
-//       next();
-//     } catch (error) {
-//       return response(res, false, null, JSON.stringify(error), 400)
-//     }
-//   }
+      req.validated = value;
+      const check = await PeriodeService.checkData(req);
+      if (check.length < 1)
+        return response(res, false, null, "bukan periode upload", 400);
+      const ext = GetFileExtention(value.file);
+      let { periode, id: periode_id } = check[0];
+      periode = `p-${periode.split(" ")[1]}`;
+      const filename = `${periode}-${Date.now()}-${
+        value.outlet_id
+      }${ext}`;
+      // const path = "/" + filename;
+      const path = config.pathRegistration + "/" + filename;
+      req.validated = { ...req.validated, filename, periode_id, path };
+      delete req.validated.file;
+      console.log(req.validated)
+      // const uploaded = await RegistrationService.getRegistrationForm(req);
+      // if (uploaded.length > 0) {
+      //   const { status_registrasi: status } = uploaded[0];
+      //   if (status === 7 || status === 8) {
+      //     return response(
+      //       res,
+      //       false,
+      //       null,
+      //       "Registration form was validated",
+      //       400
+      //     );
+      //   } else {
+      //     const { id, filename } = uploaded[0];
+      //     await FileSystem.DeleteFile(`${config.pathRegistration}/${filename}`);
+      //     await FileSystem.WriteFile(path, value.file, true);
+      //     req.validated.id = id;
+      //     await RegistrationService.update(req);
+      //     return response(
+      //       res,
+      //       true,
+      //       "success post registration form",
+      //       null,
+      //       200
+      //     );
+      //   }
+      // }
+      await FileSystem.WriteFile(path, value.file, false);
+      return response(res, true, "oke", null, 200)
+      next();
+    } catch (error) {
+      return response(res, false, null, JSON.stringify(error), 400)
+    }
+  }
 //   async validation(
 //     req: Request,
 //     res: Response,
