f1c962a6dd7138d331831a9ff62dc03da10478e0
prioarief <prioariefgunawangunawan@gmail.com>
2021-10-06 14:36:25 +0700

    refactor get region

--- src/services/Region.ts
+++ src/services/Region.ts
@@ -1,64 +1,19 @@
 import { Request } from 'express';
 import { QueryTypes } from 'sequelize';
 import db from '../config/db';
+import FilterParams from '../helpers/FilterParams';
 
 class Region {
 	async get(req: Request): Promise<any> {
-		let {
-			outlet_id,
-			area_id,
-			wilayah_id,
-			distributor_id,
-			region_id,
-			ass_id,
-			asm_id,
-			salesman_id,
-		} = req.validated;
-		const { scope, level } = req.decoded;
-		let addWhere: string | null = null;
-		let params: string[] = [];
-		if (level === '4') addWhere = 'o.distributor_id';
-		if (level === '2') addWhere = 'o.region_id';
-		if (level === '3') addWhere = 'o.city_id_alias';
-		if (level === '5') addWhere = 'o.outlet_id';
 	
 		let query =
-			'select distinct r.nama_pulau_alias as region_name, r.pulau_id_alias as region_id FROM ms_pulau_alias as r INNER JOIN mstr_outlet as o on r.pulau_id_alias = o.region_id INNER JOIN ms_user_scope as us on o.outlet_id = us.scope INNER JOIN ms_dist_pic as dp on o.distributor_id = dp.distributor_id WHERE r.pulau_id_alias IS NOT NULL';
+			'select distinct reg.nama_pulau_alias as region_name, reg.pulau_id_alias as region_id FROM ms_pulau_alias as reg INNER JOIN mstr_outlet as o on reg.pulau_id_alias = o.region_id INNER JOIN ms_user_scope as us on o.outlet_id = us.scope INNER JOIN ms_dist_pic as dp on o.distributor_id = dp.distributor_id WHERE reg.pulau_id_alias IS NOT NULL';
+
+		let {query: newQuery, params} = FilterParams.query(req, query)
 	
-		if (addWhere) {
-			query += ' AND ' + addWhere + ' IN (?)';
-			params.push(scope);
-		}
-		if (distributor_id) {
-			query += ' AND o.distributor_id = ?';
-			params.push(distributor_id);
-		}
-		if (region_id) {
-			query += ' AND o.region_id = ?';
-			params.push(region_id);
-		}
-		if (outlet_id) {
-			query += ' AND o.outlet_id = ?';
-			params.push(outlet_id);
-		}
-		if (area_id) {
-			query += ' AND o.city_id_alias = ?';
-			params.push(area_id);
-		}
-		if (wilayah_id) {
-			query += ' AND r.head_region_id = ?';
-			params.push(wilayah_id);
-		}
-		if (ass_id) {
-			query += ' AND dp.ass_id = ?';
-			params.push(ass_id);
-		}
-		if (asm_id) {
-			query += ' AND dp.asm_id = ?';
-			params.push(asm_id);
-		}
+		
 	
-		return await db.query(query + ' order by r.pulau_id_alias ASC', {
+		return await db.query(newQuery + ' order by reg.pulau_id_alias ASC', {
 			raw: true,
 			type: QueryTypes.SELECT,
 			replacements: params,
