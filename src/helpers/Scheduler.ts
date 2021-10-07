f373ccbabdc36ea5ba7cdd4be506d11fe950e469
prioarief <prioariefgunawangunawan@gmail.com>
2021-10-07 14:44:21 +0700

    fix scheduler for reschedule task

--- src/helpers/Scheduler.ts
+++ src/helpers/Scheduler.ts
@@ -27,21 +27,21 @@ class Scheduler {
   }
   reschedule(oldDate: string, date: string, cb: any) {
     const rule = new schedule.RecurrenceRule();
-    rule.date = +date.split("-")[2];
-    rule.month = +date.split("-")[1] - 1;
-    rule.year = +date.split("-")[0];
-    rule.minute = 0;
-    rule.second = 0;
-    rule.hour = 0;
-    const newRule = new schedule.RecurrenceRule();
     rule.date = +oldDate.split("-")[2];
     rule.month = +oldDate.split("-")[1] - 1;
     rule.year = +oldDate.split("-")[0];
     rule.minute = 0;
     rule.second = 0;
     rule.hour = 0;
+    const newRule = new schedule.RecurrenceRule();
+    newRule.date = +date.split("-")[2];
+    newRule.month = +date.split("-")[1] - 1;
+    newRule.year = +date.split("-")[0];
+    newRule.minute = 0;
+    newRule.second = 0;
+    newRule.hour = 0;
 
-    schedule.scheduleJob(rule, () => cb()).reschedule(newRule);
+    schedule.scheduleJob(rule, () => cb())?.reschedule(newRule);
     return true;
   }
 }
