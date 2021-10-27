5bb6bdfa55ed4a08e61e89d70aadff21f8f8962d
prioarief <prioariefgunawangunawan@gmail.com>
2021-10-27 15:26:31 +0700

    get point's outlet when get product

--- src/controllers/Redeem.ts
+++ src/controllers/Redeem.ts
@@ -109,11 +109,14 @@ class Redeem {
     try {
       let outletPoint = await Service.getPointByOutlet(req);
       let outletPointRedeem = await Service.getPointRedeemByOutlet(req);
-      let diff =
-        parseFloat(outletPoint[0]?.achieve || 0) -
-        parseFloat(outletPointRedeem[0]?.redeem || 0);
+      let diff = parseFloat(
+        (
+          parseFloat(outletPoint[0]?.achieve || 0) -
+          parseFloat(outletPointRedeem[0]?.redeem || 0)
+        ).toFixed(2)
+      );
       let product = await Service.getProduct(req, diff);
-      return response(res, true, product, null, 200);
+      return response(res, true, { point: diff, product }, null, 200);
     } catch (error) {
       console.log(error);
     }
