5a08128267e028c5ca068424a613a09a637c6135
prioarief <prioariefgunawangunawan@gmail.com>
2021-11-03 14:45:39 +0700

    fix region helper

--- src/helpers/RegistrationHelper.ts
+++ src/helpers/RegistrationHelper.ts
@@ -1,35 +1,46 @@
+import _ from "lodash";
+
 class RegistrationHelper {
   index(data: any[], type: string): any[] {
+    const sumDataBy = (data: any[], key: string) =>
+      _.sumBy(data, (o) => o[key]);
     data = data.map((e: any) => ({
       ...e,
       Level1: e.notregist,
-      Level1percent: ((e.notregist/e.total) * 100).toFixed(2) + '%',
+      Level1percent: ((e.notregist / e.total) * 100).toFixed(2) + "%",
       percentage: e.pencapaian + "%",
       pencapaian: parseFloat(e.pencapaian),
     }));
 
     const summ = {
       [type]: "Total Pencapaian",
-      notregist: data.reduce((prev, curr) => prev + curr.notregist, 0),
-      regist: data.reduce((prev, curr) => prev + curr.regist, 0),
-      total: data.reduce((prev, curr) => prev + curr.total, 0),
+      notregist: sumDataBy(data, "notregist"),
+      regist: sumDataBy(data, "regist"),
+      total: sumDataBy(data, "total"),
+      Level1: sumDataBy(data, "Level1"),
+      Level2: sumDataBy(data, "Level2"),
+      Level3: sumDataBy(data, "Level3"),
+      Level4: sumDataBy(data, "Level4"),
+      Level1percent: ((sumDataBy(data, "Level1")/sumDataBy(data, "total")) * 100).toFixed(2) + "%",
+      Level2percent: ((sumDataBy(data, "Level2")/sumDataBy(data, "total")) * 100).toFixed(2) + "%",
+      Level3percent: ((sumDataBy(data, "Level3")/sumDataBy(data, "total")) * 100).toFixed(2) + "%",
+      Level4percent: ((sumDataBy(data, "Level4")/sumDataBy(data, "total")) * 100).toFixed(2) + "%",
       percentage:
-        (
-          (data.reduce((prev, curr) => prev + curr.regist, 0) /
-            data.reduce((prev, curr) => prev + curr.total, 0)) *
-          100
-        ).toFixed(2) + "%",
+        ((sumDataBy(data, "regist") / sumDataBy(data, "total")) * 100).toFixed(
+          2
+        ) + "%",
       pencapaian: parseFloat(
         (
-          (data.reduce((prev, curr) => prev + curr.regist, 0) /
-            data.reduce((prev, curr) => prev + curr.total, 0)) *
+          (sumDataBy(data, "regist") /
+            sumDataBy(data, "total")) *
           100
         ).toFixed(2)
       ),
       totals: data[0]?.totals,
       bobot_outlet:
         (
-          (data.reduce((prev, curr) => prev + curr.total, 0) / data[0]?.totals) *
+          (sumDataBy(data, "total") /
+            data[0]?.totals) *
           100
         ).toFixed(2) + "%",
     };
