0854bdf1c7b38e771f66cf4dd8f4be22e21043e9
prioarief <prioariefgunawangunawan@gmail.com>
2021-12-06 09:53:26 +0700

    add month filter on get point activity report

--- src/request/Report.ts
+++ src/request/Report.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import joi from "joi";
+import App from "../helpers/App";
 import response from "../helpers/Response";
 
 class Report {
   get(req: Request, res: Response, next: NextFunction): any {
+    const monthsId = App.months.map((e: any) => e.id)
     const schema = joi.object({
         show: joi.number().valid(10, 15),
         page: joi.number(),
+        month: joi.number().valid(...monthsId),
     });
 
     const { value, error } = schema.validate({...req.query, ...req.body});
